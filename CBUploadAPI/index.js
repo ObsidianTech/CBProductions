db0bf43aba8cccd7383eee6ffe47e8dad764d186
Christopher Smith <csmith4work@gmail.com>
2019-03-07 20:15:20 -0600

    Testing file upload

--- CBUploadAPI/index.js
+++ CBUploadAPI/index.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-
+const multiparty = require('connect-multiparty'),
+  multipartyMiddleware = multiparty();
+const fs = require('fs');
+const s3fs = require('s3fs');
 const app = express();
 
-app.use(bodyParser.json());
-app.use(cors());
+//configure s3fs
+const s3fsImplementation = new s3fs('nurenbucket', {
+  accessKeyId: 'AKIAI5EFEDJVYMDKKPGA',
+  secretAccessKey: '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'
+})
 
-app.get('/', (req, res) =>{
-    res.send("Hello from Upload API!");
+//configure app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(multipartyMiddleware);
+
+//API Routes
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + 'public/index.html');
 });
 
-app.listen(process.env.PORT || 5001);
+app.post('/test', (req, res) => {
+  var file = req.files.file;
+  var stream = fs.createReadStream(file.path);
+  return s3fsImplementation.writeFile(file.originalFileName, stream).then(function(){
+    fs.unlink(file.path, function(err){
+      if(err){
+        console.error(err);
+      }
+    });
+    res.send('Uploaded!!');
+  });
+});
+
+app.listen(process.env.PORT || 5001, () => console.log('server started'));
